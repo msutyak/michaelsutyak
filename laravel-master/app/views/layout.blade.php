c60b97e48761bcf6a873719cd42cc3af6a057ee1
msutyak <msutyak@gmail.com>
2013-10-30 20:55:17 -0700

    Worked on Layout and added CSS

---
+++ laravel-master/app/views/layout.blade.php
@@ -0,0 +1,15 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://ogp.me/ns/fb#">
+
+<head>
+<meta charset="utf-8">
+<link rel="shortcut icon" href='/assets/favicon.ico?'>
+<title>Michael Sutyak</title>
+{{ HTML::style('/assets/CSS/style.css') }}
+{{ HTML::style('//cloud.webtype.com/css/4bf995be-bdb8-4e20-bdf3-30fb58c57828.css') }}
+
+{{ HTML::script('http://code.jquery.com/jquery-latest.js') }}
+</head>
+<body>
+
+</body>
+</html>
