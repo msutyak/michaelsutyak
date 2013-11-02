97430684159adea71ed7bf7844efa52bf18269f5
msutyak <msutyak@gmail.com>
2013-11-02 11:34:43 -0700

    Finished main layout

--- laravel-master/app/views/home.blade.php
+++ laravel-master/app/views/home.blade.php
@@ -7,29 +7,27 @@
 <div id="container-center">
 	<img id="headshot" src="/assets/images/Profile_Icon.png" width="130" height="130" />
 	<h3 id="name-and-title"><span>Michael Sutyak</span></h3>
-					
-		<hr id="hr-index-top" class="top">
-
-		<p>
-		Michael is currently a User Acquisition Manager at <a href="http://facebook.com/design" target="_blank" class="index_link">KIXEYE</a> in <a href="http://g.co/maps/eccmv" target="_blank" class="index_link">San Francisco, California,</a><br>
-		where he co-founded the <a href="http://facebook.com/analoglab" target="_blank" class="index_link">Analog Research Laboratory</a>.</br>
-		Prior to Facebook, he worked with <a href="http://thedecoderring.com/" target="_blank" class="index_link">Decoder Ring</a> / <a href="http://helmsworkshop.com" target="_blank" class="index_link">Helms Workshop</a><br> 
-		in <a href="http://g.co/maps/ssztb" target="_blank" class="index_link">Austin, Texas</a>. He is also an alumni and advisor of <a href="http://projectmlab.com/" target="_blank" class="index_link">Project M</a>.
-		</p>
-		<br>
+		<br>	
 		<div class="small_caps">
 		<a href="mailto:msutyak@gmail.com"><img src="/assets/images/email_icon.png" class="social-icons" height="22px"></a>
-		&nbsp;&middot;&nbsp;
+		&nbsp;&nbsp;
 		<a href="https://www.linkedin.com/in/michaelsutyak"><img src="/assets/images/linkedin_icon.jpg" class="social-icons" height="22px"></a>
-		&nbsp;&middot;&nbsp;
+		&nbsp;&nbsp;
 		<a href="https://twitter.com/msutyak"><img src="/assets/images/twitter_icon.png" class="social-icons" height="22px"></a>
-		&nbsp;&middot;&nbsp;
+		&nbsp;&nbsp;
 		<a href="https://www.facebook.com/michael.sutyak"><img src="/assets/images/facebook_icon.png" class="social-icons" height="22px"></a>
-		&nbsp;&middot;&nbsp;
-		<img src="/assets/images/instagram_icon.png" class="social-icons" height="22px">
-		&nbsp;&middot;&nbsp;
-		<img src="/assets/images/wordpress_icon.png" class="social-icons" height="22px">
+		&nbsp;&nbsp;
+		<a href="http://instagram.com/msutyak"><img src="/assets/images/instagram_icon.png" class="social-icons" height="22px"></a>
+		&nbsp;&nbsp;
+		<a href="http://michaelsutyak.wordpress.com/"><img src="/assets/images/wordpress_icon.png" class="social-icons" height="22px"></a>
 		</div>
+
+		<p>
+		Michael is currently a User Acquisition Manager at <a href="https://www.kixeye.com" target="_blank" class="index_link">KIXEYE</a> in <a href="http://goo.gl/maps/II5G3" target="_blank" class="index_link">San Francisco, California,</a><br>
+		where he controls ad spend for multiple mobile and browser-based titles.</br>
+		He also enjoys building and growing consumer facing web and mobile products,</br> 
+		particularly using the <a href="http://laravel.com/" target="_blank" class="index_link">Laravel PHP Framework</a>.
+		</p>
 
 		<hr id="hr-index-middle" class="bottom">
 </div>
