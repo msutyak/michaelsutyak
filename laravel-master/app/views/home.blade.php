34f31bd2e4b92f7dfc84cfa82027494f180b63b3
msutyak <michaelsutyak@Michael-Sutyaks-MacBook-Pro.local>
2015-03-19 21:16:41 -0700

    remove wordpress

--- laravel-master/app/views/home.blade.php
+++ laravel-master/app/views/home.blade.php
@@ -18,15 +18,13 @@
 		<a href="https://www.facebook.com/michael.sutyak" target="_blank"><img src="/assets/images/facebook_icon.png" class="social-icons" height="22px"></a>
 		&nbsp;&nbsp;
 		<a href="http://instagram.com/msutyak" target="_blank"><img src="/assets/images/instagram_icon.png" class="social-icons" height="22px"></a>
-		&nbsp;&nbsp;
-		<a href="http://michaelsutyak.wordpress.com/" target="_blank"><img src="/assets/images/wordpress_icon.png" class="social-icons" height="22px"></a>
 		</div>
 
 		<p>
-		Michael is currently a User Acquisition Manager at <a href="https://www.kixeye.com" target="_blank" class="index_link">KIXEYE</a> in <a href="http://goo.gl/maps/II5G3" target="_blank" class="index_link">San Francisco, California,</a><br>
+		Michael is currently a Senior User Acquisition Manager at <a href="https://www.kixeye.com" target="_blank" class="index_link">KIXEYE</a> in <a href="http://goo.gl/maps/II5G3" target="_blank" class="index_link">San Francisco, California,</a><br>
 		where he controls ad spend for multiple mobile and browser-based titles.</br>
 		He also enjoys building and growing consumer facing web and mobile products,</br> 
-		particularly using the <a href="http://laravel.com/" target="_blank" class="index_link">Laravel PHP Framework</a>.
+		and exploring new technologies.
 		</p>
 
 		<hr id="hr-index-middle" class="bottom">
