b99595794b9e80e18084ca0f3cc193fc846aaada
msutyak <michaelsutyak@Michael-Sutyaks-MacBook-Pro.local>
2015-04-24 15:49:45 -0700

    updated font-awesome

--- laravel-master/app/views/layout.blade.php
+++ laravel-master/app/views/layout.blade.php
@@ -5,7 +5,7 @@
 <link rel="shortcut icon" href='/assets/images/volcano_ico.ico?'>
 <title>Michael Sutyak</title>
 {{ HTML::style('/assets/CSS/style.css') }}
-{{ HTML::style('//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css') }}
+{{ HTML::style('//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css') }}
 
 {{ HTML::script('http://code.jquery.com/jquery-latest.js') }}
 <!-- start Mixpanel --><script type="text/javascript">(function(f,b){if(!b.__SV){var a,e,i,g;window.mixpanel=b;b._i=[];b.init=function(a,e,d){function f(b,h){var a=h.split(".");2==a.length&&(b=b[a[0]],h=a[1]);b[h]=function(){b.push([h].concat(Array.prototype.slice.call(arguments,0)))}}var c=b;"undefined"!==typeof d?c=b[d]=[]:d="mixpanel";c.people=c.people||[];c.toString=function(b){var a="mixpanel";"mixpanel"!==d&&(a+="."+d);b||(a+=" (stub)");return a};c.people.toString=function(){return c.toString(1)+".people (stub)"};i="disable track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.increment people.append people.union people.track_charge people.clear_charges people.delete_user".split(" ");
