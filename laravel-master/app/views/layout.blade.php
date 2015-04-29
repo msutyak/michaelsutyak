20a49383163b53152fa04f68fff0a1459da1cfb3
msutyak <michaelsutyak@Michael-Sutyaks-MacBook-Pro.local>
2015-04-28 21:40:31 -0700

    changed mixpanel code

--- laravel-master/app/views/layout.blade.php
+++ laravel-master/app/views/layout.blade.php
@@ -10,7 +10,7 @@
 {{ HTML::script('/assets/js/jquery-2.1.3.min.js') }}
 <!-- start Mixpanel --><script type="text/javascript">(function(f,b){if(!b.__SV){var a,e,i,g;window.mixpanel=b;b._i=[];b.init=function(a,e,d){function f(b,h){var a=h.split(".");2==a.length&&(b=b[a[0]],h=a[1]);b[h]=function(){b.push([h].concat(Array.prototype.slice.call(arguments,0)))}}var c=b;"undefined"!==typeof d?c=b[d]=[]:d="mixpanel";c.people=c.people||[];c.toString=function(b){var a="mixpanel";"mixpanel"!==d&&(a+="."+d);b||(a+=" (stub)");return a};c.people.toString=function(){return c.toString(1)+".people (stub)"};i="disable track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.increment people.append people.union people.track_charge people.clear_charges people.delete_user".split(" ");
 for(g=0;g<i.length;g++)f(c,i[g]);b._i.push([a,e,d])};b.__SV=1.2;a=f.createElement("script");a.type="text/javascript";a.async=!0;a.src="undefined"!==typeof MIXPANEL_CUSTOM_LIB_URL?MIXPANEL_CUSTOM_LIB_URL:"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";e=f.getElementsByTagName("script")[0];e.parentNode.insertBefore(a,e)}})(document,window.mixpanel||[]);
-mixpanel.init("d1c56777a212571d28a2b3e18a905418");</script><!-- end Mixpanel -->
+mixpanel.init("682f2c5bd6b59d5d7f52fcc86d23498f");</script><!-- end Mixpanel -->
 </head>
 <body>
 @yield('container')
