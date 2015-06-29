4452a4fde4d1f4e77c093afb3851ac054543df6c
msutyak <michaelsutyak@Michael-Sutyaks-MacBook-Pro.local>
2015-06-29 13:35:59 -0700

    changed work position

--- laravel-master/app/views/home.blade.php
+++ laravel-master/app/views/home.blade.php
@@ -23,8 +23,8 @@
 		</div>
 
 		<p>
-		Michael is currently a Senior User Acquisition Manager at <a href="https://www.kixeye.com" target="_blank" class="index_link">KIXEYE</a> in <a href="http://goo.gl/maps/II5G3" target="_blank" class="index_link">San Francisco, California,</a><br>
-		where he controls ad spend for multiple mobile and browser-based titles.</br>
+		Michael is currently a Senior Acquisition Marketing Manager at <a href="https://www.studyblue.com" target="_blank" class="index_link">StudyBlue</a> in <a href="http://goo.gl/maps/II5G3" target="_blank" class="index_link">San Francisco, California,</a><br>
+		where he leads the growth effort for web and mobile apps.</br>
 		He also enjoys building and growing consumer facing web and mobile products,</br> 
 		and exploring new technologies.
 		</p>
